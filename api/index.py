b834bcd06fe53d2f401567b03a916e9037a46e8b
vinicius <viniciusfod@gmail.com>
2024-09-24 10:46:39 -0300

    cors

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from translate import Translator
 
 translator = Translator(from_lang="pt", to_lang="en")
@@ -7,20 +7,34 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-  return 'Hello, World!'
+    response = make_response('Hello, World!')
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Permitir todas as origens
+    return response
 
 @app.route('/', methods=['POST'])
 def home():
     data = request.get_json()
 
     if not data or 'texto' not in data:
-        return jsonify({"error": "Dados inválidos ou incompletos"}), 400
+        response = jsonify({"error": "Dados inválidos ou incompletos"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response, 400
 
     texto = data['texto']
-
     translation = translator.translate(texto)
 
-    return jsonify({"texto": translation}), 201
+    response = jsonify({"texto": translation})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response, 201
+
+# Pré-vôo (OPTIONS) para permitir CORS em requisições POST/PUT/DELETE, etc.
+@app.route('/', methods=['OPTIONS'])
+def options():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    return response
 
 # Iniciando o servidor
 if __name__ == '__main__':
