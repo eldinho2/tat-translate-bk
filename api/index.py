c35a2e9b24a2ccc9c8e083953a6eb01429aa596e
vinicius <viniciusfod@gmail.com>
2024-09-24 10:42:57 -0300

    cors

--- api/index.py
+++ api/index.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
 from translate import Translator
 
 translator = Translator(from_lang="pt", to_lang="en")
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/', methods=['GET'])
-@cross_origin()
 def index():
-    return 'Hello, World!'
+  return 'Hello, World!'
 
 @app.route('/', methods=['POST'])
-@cross_origin()
 def home():
     data = request.get_json()
 
@@ -21,6 +17,7 @@ def home():
         return jsonify({"error": "Dados inválidos ou incompletos"}), 400
 
     texto = data['texto']
+
     translation = translator.translate(texto)
 
     return jsonify({"texto": translation}), 201
